d3f040486e398b3a799217282b9510e4a91f40d8
Your Name <you@example.com>
2016-06-11 13:33:07 +0600

    Just test

--- app/src/main/java/smarthome/com/bl/Updator.java
+++ app/src/main/java/smarthome/com/bl/Updator.java
@@ -4,6 +4,9 @@ package smarthome.com.bl;
  * Created by Azad on 03-Jun-16.
  */
 public interface Updator {
+    /*
+    Update UI
+     */
     void update();
     public void setData(String data);
 }
